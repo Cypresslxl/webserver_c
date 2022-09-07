aba4380d9e4be4153c89aa01ae61c751d38be112
Cypresslxl <97680411+Cypresslxl@users.noreply.github.com>
2022-09-07 12:13:37 +0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Evila on 2021/6/27.
+// Created by Cypress on 2022/6/27.
 //
 
 #include <stdio.h>
@@ -17,40 +17,40 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-        // 1. 建立一个Socket
-        int _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (_sock < 0)
+        // 1. 建立一个监听的 socket fd
+        int lfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (lfd < 0)
         {
-                cout << "create socket error: " << strerror(errno) << "%s(errno: " << errno << endl;
+                perror("socket");
                 exit(0);
         }
 
         // 2. 设置请求连接的server 地址
-        sockaddr_in _sevrAddr = {};    // sockaddr_in为网络地址的结构体
-        _sevrAddr.sin_family = AF_INET;
-        int port = 45678;
-        _sevrAddr.sin_port = htons(port);
+        sockaddr_in sock_in = {};    // sockaddr_in为网络地址的结构体
+        sock_in.sin_family = AF_INET;
+        int port = 10000;
+        sock_in.sin_port = htons(port);
 
-        string server_ip;
-        cout << "please input server ip: ";
-        cin >> server_ip;
+        string ip;
+        cout << "please input your server ip: ";
+        cin >> ip;
 
         if (inet_pton(AF_INET, server_ip.c_str(), &_sevrAddr.sin_addr) <= 0)
         {
-                cout <<"inet_pton error for " << server_ip << endl;
+                perror("inet_pton");
                 exit(0);
         }
 
         // 3. 连接服务端
-        int ret = connect(_sock, (struct sockaddr*)&_sevrAddr, sizeof(_sevrAddr));
+        int ret = connect(lfd, (struct sockaddr*)&sock_in, sizeof(sock_in));
         if (ret < 0)
         {
-                cout << "connect error: " << strerror(errno) << "errno: " << errno << endl;
+                perror("connect");
                 exit(0);
         }
         else
         {
-                cout << "connect to server: " << server_ip << ":" << port << " success!" << endl;
+                cout << "connect to server: " << ip << ":" << port << " success!" << endl;
         }
 
         // 4. 向服务端发送数据
@@ -60,13 +60,13 @@ int main(int argc, char** argv)
             cout << "please input send data: ";
             getchar();
             getline(cin, sendData);
-            if(send(_sock, sendData.c_str(), sendData.length(), 0) < 0)
+            if(send(lfd, sendData.c_str(), sendData.length(), 0) < 0)
             {
-                    cout << "send msg error: " << strerror(errno) << "errno: " << errno << endl;
+                    perror("send");
                     exit(0);
             }
         }
 
-        close(_sock);
-        exit(0);
+        close(lfd);
+        return 0;
 }
